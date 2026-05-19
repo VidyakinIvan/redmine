c5fc2c43f1e7b54b643b9a3dd3c5ee1e4de376c7
Vidyakin Ivan <ivan.vidyakin@ant-tech.ru>
2026-05-19 17:28:44 +0300

    Fix plugin for main page redirect

--- plugins/redmine_login_redirect/init.rb
+++ plugins/redmine_login_redirect/init.rb
@@ -1,8 +1,17 @@
 Redmine::Plugin.register :redmine_login_redirect do
   name 'Custom Login Redirect'
-  author 'Ivan Vidyakin'
+  author 'Your Company'
   description 'Redirect users to the home page after login'
-  version '0.0.1'
+  version '0.0.2'
+end
 
-  require_dependency 'account_controller_patch'
+Rails.application.config.to_prepare do
+  require_dependency 'account_controller'
+  AccountController.class_eval do
+    def successful_authentication_with_redirect(user)
+      call_hook(:controller_account_success_authentication_after, { :user => user })
+      redirect_to home_url
+    end
+    alias_method :successful_authentication, :successful_authentication_with_redirect
+  end
 end
