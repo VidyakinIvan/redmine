4f366f7697108a2172ba71185d01cd3b8d26f5f7
Vidyakin Ivan <ivan.vidyakin@ant-tech.ru>
2026-05-19 16:47:59 +0300

    Create plugin for custom help button

---
+++ plugins/redmine_custom_help/init.rb
@@ -0,0 +1,13 @@
+Redmine::Plugin.register :custom_help do
+  name 'Custom Help Plugin'
+  author 'Ivan Vidyakin'
+  description 'Replace default Help with a local wiki page'
+  version '0.0.1'
+  
+  delete_menu_item :top_menu, :help
+
+  menu :top_menu, :custom_help_link, 
+       { :controller => 'wiki', :action => 'show', :project_id => 'help'},
+       :caption => 'Помощь',
+       :last => true
+end
