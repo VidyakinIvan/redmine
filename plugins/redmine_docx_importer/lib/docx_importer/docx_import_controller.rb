41f9b7c7030cfe1a65ae184a533000f7ffd2c12a
Vidyakin Ivan <ivan.vidyakin@ant-tech.ru>
2026-06-08 13:53:47 +0300

    Change controller for new headers

--- plugins/redmine_docx_importer/lib/docx_importer/docx_import_controller.rb
+++ plugins/redmine_docx_importer/lib/docx_importer/docx_import_controller.rb
@@ -37,36 +37,38 @@ class DocxImportController < ApplicationController
 
   def import_from_csv(csv_string)
     count = 0
-    number_to_issue = {}
 
     CSV.parse(csv_string, headers: true, col_sep: ';') do |row|
-      next if row['Тема задачи'].blank?
+      subject = row['Тема']
+      next if subject.blank?
+
+      description = row['Описание'] || ''
+      status_name = row['Статус'] || 'Бэклог'
+      tracker_name = row['Трекер'] || 'Входящий поток'
+      priority_name = row['Приоритет'] || 'Важная'
+
+      status = IssueStatus.find_by(name: status_name)
+      tracker = Tracker.find_by(name: tracker_name)
+      priority = IssuePriority.find_by(name: priority_name)
+
+      status ||= IssueStatus.default
+      tracker ||= @project.trackers.first
+      priority ||= IssuePriority.default
 
       issue = Issue.new(
         project: @project,
-        tracker: @project.trackers.first,
+        tracker: tracker,
         author: User.current,
-        subject: row['Тема задачи'],
-        description: row['Описание']
+        status: status,
+        priority: priority,
+        subject: subject,
+        description: description
       )
 
       if issue.save
         count += 1
-        number_to_issue[row['Номер раздела']] = issue.id
-      end
-    end
-
-    CSV.parse(csv_string, headers: true, col_sep: ';') do |row|
-      parent_number = row['Родительский раздел']
-      next if parent_number.blank?
-
-      issue_id = number_to_issue[row['Номер раздела']]
-      parent_id = number_to_issue[parent_number]
-
-      if issue_id && parent_id
-        issue = Issue.find(issue_id)
-        issue.parent_issue_id = parent_id
-        issue.save
+      else
+        Rails.logger.error "DOCX Import: Failed to create issue '#{subject}': #{issue.errors.full_messages}"
       end
     end
 
