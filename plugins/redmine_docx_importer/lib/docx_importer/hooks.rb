3c668bef11039d33a65f46f11bfbff7693bf5813
Vidyakin Ivan <ivan.vidyakin@ant-tech.ru>
2026-06-22 12:47:47 +0300

    Add empty project protection

--- plugins/redmine_docx_importer/lib/docx_importer/hooks.rb
+++ plugins/redmine_docx_importer/lib/docx_importer/hooks.rb
@@ -2,6 +2,8 @@ class DocxImporterHooks < Redmine::Hook::ViewListener
   def view_issues_index_bottom(context = {})
     project = context[:project]
     controller = context[:controller]
+	
+	return unless project.present?
     controller.send(:render_to_string, {
       partial: 'docx_importer/upload_form',
       locals: { project: project }
